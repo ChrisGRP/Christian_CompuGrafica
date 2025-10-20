15becd6ce41295eac6068effc898932dedd627e6
ChrisGRP <89955399+ChrisGRP@users.noreply.github.com>
2025-10-20 08:56:26 -0600

    Entrega de la práctica 8 y actividad previa 9

--- ConfigInicial/Iluminacion.cpp
+++ ConfigInicial/Iluminacion.cpp
@@ -1,5 +1,9 @@
-
+// Romero Pizano Christian Gustavo
+// Practica 8
+// 319225596
+// 17 de septiembre del 2025
 // Std. Includes
+
 #include <string>
 
 // GLEW
@@ -21,6 +25,13 @@
 // Other Libs
 #include "SOIL2/SOIL2.h"
 #include "stb_image.h"
+#include <vector>
+
+// Definir PI 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 // Properties
 const GLuint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
@@ -38,13 +49,76 @@ GLfloat lastX = 400, lastY = 300;
 bool firstMouse = true;
 
 
-// Light attributes
-glm::vec3 lightPos(0.5f, 0.5f, 2.5f);
-float movelightPos = 0.0f;
+// variables de la luz
+glm::vec3 lightPos(0.0f, 0.0f, -5.0f); //centro del arco
+float anguloLuz = 0.0f;
+float radioArco = 3.0f;
+
+
+bool esDeDia = true; // variable para alternar entre el dia y la noche
+
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 float rot = 0.0f;
 bool activanim = false;
+
+//esfera
+void generateSphere(GLfloat radius, GLint sectors, GLint stacks,
+    std::vector<GLfloat>& vertices, std::vector<GLuint>& indices) {
+    vertices.clear();
+    indices.clear();
+
+    GLfloat x, y, z, xy;
+    GLfloat nx, ny, nz, lengthInv = 1.0f / radius;
+    GLfloat sectorStep = 2 * M_PI / sectors;
+    GLfloat stackStep = M_PI / stacks;
+    GLfloat sectorAngle, stackAngle;
+
+    // Generar vértices (Posición + Normal)
+    for (GLint i = 0; i <= stacks; ++i) {
+        stackAngle = M_PI / 2 - i * stackStep;
+        xy = radius * cosf(stackAngle);
+        z = radius * sinf(stackAngle);
+
+        for (GLint j = 0; j <= sectors; ++j) {
+            sectorAngle = j * sectorStep;
+
+            x = xy * cosf(sectorAngle);
+            y = xy * sinf(sectorAngle);
+            vertices.push_back(x);
+            vertices.push_back(y);
+            vertices.push_back(z);
+
+            nx = x * lengthInv;
+            ny = y * lengthInv;
+            nz = z * lengthInv;
+            vertices.push_back(nx);
+            vertices.push_back(ny);
+            vertices.push_back(nz);
+        }
+    }
+
+    // Generar índices
+    GLuint k1, k2;
+    for (GLint i = 0; i < stacks; ++i) {
+        k1 = i * (sectors + 1);
+        k2 = k1 + sectors + 1;
+
+        for (GLint j = 0; j < sectors; ++j, ++k1, ++k2) {
+            if (i != 0) {
+                indices.push_back(k1);
+                indices.push_back(k2);
+                indices.push_back(k1 + 1);
+            }
+
+            if (i != (stacks - 1)) {
+                indices.push_back(k1 + 1);
+                indices.push_back(k2);
+                indices.push_back(k2 + 1);
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -58,7 +132,7 @@ int main()
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Materiales e Iluminacion", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Practica 8 - Romero Pizano Christian Gustavo", nullptr, nullptr);
 
     if (nullptr == window)
     {
@@ -104,10 +178,15 @@ int main()
     // Load models
     Model red_dog((char*)"Models/perro/RedDog.obj");
     Model alien((char*)"Models/alien/toyUFO.obj");
+    Model cohete((char*)"Models/cohete/tripo_convert_84b8bfd5-eead-4e8a-a6ab-c9f4eae57cfb.obj");
+    Model meteoro((char*)"Models/meteoro/tripo_convert_542cf2d3-fa56-472a-8408-9e70cc4b0083.obj");
+    Model satelite((char*)"Models/satelite/tripo_convert_69d018bb-23a1-4f4b-b19b-fd2c46ff5283.obj");
+    Model planeta((char*)"Models/planeta/tripo_convert_5d7e7989-6038-49c7-8a54-9b04bf3d3b4d.obj");
+
     glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
     float vertices[] = {
-      -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
          0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
          0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
          0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -151,6 +230,7 @@ int main()
     };
 
     // First, set the container's VAO (and VBO)
+    // Este VAO/VBO es para el cubo, lo puedes dejar o borrar si no lo usas para nada más
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -164,8 +244,37 @@ int main()
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // Load textures
+    // buffers de la esfera
+    std::vector<GLfloat> sphereVertices;
+    std::vector<GLuint> sphereIndices;
+    generateSphere(0.5f, 36, 18, sphereVertices, sphereIndices);
 
+    GLuint sphereVAO, sphereVBO, sphereEBO;
+    glGenVertexArrays(1, &sphereVAO);
+    glGenBuffers(1, &sphereVBO);
+    glGenBuffers(1, &sphereEBO);
+
+    glBindVertexArray(sphereVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+    glBufferData(GL_ARRAY_BUFFER, sphereVertices.size() * sizeof(GLfloat),
+        &sphereVertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphereIndices.size() * sizeof(GLuint),
+        &sphereIndices[0], GL_STATIC_DRAW);
+
+    // Position attribute (Location 0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    // Normal attribute (Location 1)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0); // Desvincular
+
+
+    // Load textures
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -191,7 +300,6 @@ int main()
     }
     stbi_image_free(image);
 
-
     // Game loop
     while (!glfwWindowShouldClose(window))
     {
@@ -208,22 +316,36 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        
+
+
+        glm::vec3 posicionSolCalculada;
+        posicionSolCalculada.x = lightPos.x + radioArco * cos(anguloLuz);
+        posicionSolCalculada.y = lightPos.y + radioArco * sin(anguloLuz);
+        posicionSolCalculada.z = lightPos.z;
+
+
         lightingShader.Use();
         GLint lightPosLoc = glGetUniformLocation(lightingShader.Program, "light.position");
         GLint viewPosLoc = glGetUniformLocation(lightingShader.Program, "viewPos");
-        glUniform3f(lightPosLoc, lightPos.x + movelightPos, lightPos.y + movelightPos, lightPos.z + movelightPos);
+
+        glUniform3f(lightPosLoc, posicionSolCalculada.x, posicionSolCalculada.y, posicionSolCalculada.z);
         glUniform3f(viewPosLoc, camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z);
 
-
-        // Set lights properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.6f, 0.3f, 0.7f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 0.2f, 0.6f, 0.2f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 0.1f, 0.6f, 0.8f);
-
-
-
-
+        if (esDeDia)
+        {
+            // dia
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.5f, 0.5f, 0.5f);
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 1.0f, 1.0f, 1.0f);
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 1.0f, 1.0f, 1.0f);
+        }
+        else
+        {
+            // noche
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.55f, 0.55f, 0.6f);
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 0.25f, 0.25f, 0.75f);
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 0.3f, 0.3f, 0.7f);
+        }
+ 
         glm::mat4 view = camera.GetViewMatrix();
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
@@ -234,44 +356,79 @@ int main()
         glUniform3f(glGetUniformLocation(lightingShader.Program, "material.specular"), 0.5f, 0.5f, 0.5f);
         glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 0.3f);
 
-        
-
-
-
-
-
-
         // Draw the loaded model
-        glm::mat4 model(1);
-        model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f));
+        model = glm::scale(model, glm::vec3(1.4f, 1.4f, 1.4f));
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glBindVertexArray(VAO);
         red_dog.Draw(lightingShader);
 
-        model = glm::mat4(1.0f); 
-        model = glm::translate(model, glm::vec3(2.0f, 2.0f, 2.0f)); 
-        model = glm::scale(model, glm::vec3(4.5f, 4.5f, 4.5f));   
-        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        alien.Draw(lightingShader); 
+        glm::mat4 alienmodel(1.0f);
+        alienmodel = glm::translate(alienmodel, glm::vec3(1.5f, 0.0f, -5.0f));
+        alienmodel = glm::scale(alienmodel, glm::vec3(10.5f, 10.5f, 10.5f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(alienmodel));
+        alien.Draw(lightingShader);
 
-       
+        // Dibujar satelite
+        glm::mat4 sateliteModel(1.0f);
+        sateliteModel = glm::translate(sateliteModel, glm::vec3(-1.5f, 0.0f, -5.0f));
+        sateliteModel = glm::scale(sateliteModel, glm::vec3(1.4f, 1.4f, 1.4f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(sateliteModel));
+        satelite.Draw(lightingShader);
+
+        // Dibujar cohete
+        glm::mat4 coheteModel(1.0f);
+        coheteModel = glm::translate(coheteModel, glm::vec3(0.0f, 1.5f, -5.0f));
+        coheteModel = glm::rotate(coheteModel, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        coheteModel = glm::scale(coheteModel, glm::vec3(1.3f, 1.3f, 1.3f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(coheteModel));
+        cohete.Draw(lightingShader);
+
+        // Dibujar planeta
+        glm::mat4 planetaModel(1.0f);
+        planetaModel = glm::translate(planetaModel, glm::vec3(0.0f, 0.3f, -6.5f));
+        planetaModel = glm::scale(planetaModel, glm::vec3(1.1f, 1.1f, 1.1f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(planetaModel));
+        planeta.Draw(lightingShader);
+
+        // Dibujar meteoro
+        glm::mat4 meteoroModel(1.0f);
+        meteoroModel = glm::translate(meteoroModel, glm::vec3(1.5f, 1.5f, -5.0f));
+        meteoroModel = glm::scale(meteoroModel, glm::vec3(1.4f, 1.4f, 1.4f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(meteoroModel));
+        meteoro.Draw(lightingShader);
+
+
         //glDrawArrays(GL_TRIANGLES, 0, 36);
-        
-
         glBindVertexArray(0);
 
-
-
-
+        // Dibujar la esfera de luz
         lampshader.Use();
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+        // añadimos color a la esfera
+        if (esDeDia)
+        {
+            glUniform3f(glGetUniformLocation(lampshader.Program, "lampColor"), 1.0f, 1.0f, 0.7f);
+        }
+        else
+        {
+  
+            glUniform3f(glGetUniformLocation(lampshader.Program, "lampColor"), 0.7f, 0.7f, 1.0f);
+        }
+
+        // Volvemos a usar la variable 'model' para la esfera
         model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos + movelightPos);
+
+        // La movemos a  'posicionSolCalculada'
+        model = glm::translate(model, posicionSolCalculada);
         model = glm::scale(model, glm::vec3(0.3f));
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // Dibuja la esfera
+        glBindVertexArray(sphereVAO);
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(sphereIndices.size()), GL_UNSIGNED_INT, 0); 
         glBindVertexArray(0);
 
         // Swap the buffers
@@ -281,10 +438,13 @@ int main()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
+    glDeleteVertexArrays(1, &sphereVAO);
+    glDeleteBuffers(1, &sphereVBO);
+    glDeleteBuffers(1, &sphereEBO);
+
     glfwTerminate();
     return 0;
 }
-
 
 // Moves/alters the camera positions based on user input
 void DoMovement()
@@ -331,6 +491,11 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
         if (action == GLFW_PRESS)
         {
             keys[key] = true;
+            if (key == GLFW_KEY_P)
+            {
+                esDeDia = !esDeDia;
+            }
+
         }
         else if (action == GLFW_RELEASE)
         {
@@ -338,16 +503,25 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
         }
     }
 
+    // Control del angulo del sol
     if (keys[GLFW_KEY_O])
     {
-       
-        movelightPos += 0.1f;
+
+        anguloLuz += 0.02f;
+        if (anguloLuz > M_PI) // limite de 180 grados
+        {
+            anguloLuz = M_PI;
+        }
     }
 
     if (keys[GLFW_KEY_L])
     {
-        
-        movelightPos -= 0.1f;
+
+        anguloLuz -= 0.02f;
+        if (anguloLuz < 0.0f) //  limite de 0 grados
+        {
+            anguloLuz = 0.0f;
+        }
     }
 
 
@@ -370,5 +544,3 @@ void MouseCallback(GLFWwindow* window, double xPos, double yPos)
 
     camera.ProcessMouseMovement(xOffset, yOffset);
 }
-
-
